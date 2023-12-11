fba388491ac7ab8e3c2cf2005216391b35cffc56
Pete Brooks <pete@txidigital.com>
2023-12-11 12:14:25 -0600

    [WIP] refactor multiprocess

--- purr/clumps/text/html_combine.py
+++ purr/clumps/text/html_combine.py
@@ -7,81 +7,95 @@ from bs4 import BeautifulSoup
 
 app = typer.Typer(invoke_without_command=True)
 
-def html_to_markdown(html_content: str) -> str:
-    soup = BeautifulSoup(html_content, 'html.parser')
-    body = soup.find('body')
-    return md(str(body)) if body else md(html_content)
 
-def process_file(file_path: str, minify: bool, convert_to_md: bool) -> str:
-    with open(file_path, "r") as f:
-        content = f.read()
+def convert_html_to_markdown(html_content: str) -> str:
+    """
+    Converts HTML content to Markdown.
+    """
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.find("body") or html_content
+    return md(str(body_content))
+
+
+def process_file(file_path: str, minify: bool, convert_to_markdown: bool) -> str:
+    """
+    Processes an individual HTML file - reads, optionally minifies and/or converts to Markdown.
+    """
+    with open(file_path, "r") as file:
+        content = file.read()
         if minify:
             content = htmlmin.minify(content, remove_empty_space=True)
-        if convert_to_md:
-            content = html_to_markdown(content)
+        if convert_to_markdown:
+            content = convert_html_to_markdown(content)
         return content
+
 
 def combine_files(
     input_dir: str,
-    output_dir: str = None,
-    max_size: int = 80 * 1024 * 1024,
-    buffer_size: int = 1024 * 1024,
-    minify: bool = False,
-    convert_to_md: bool = False,
+    output_dir: str,
+    max_size: int,
+    buffer_size: int,
+    minify: bool,
+    convert_to_markdown: bool,
 ):
-    if output_dir is None:
-        output_dir = os.getcwd()
+    """
+    Combines HTML files from a directory into single files of a maximum size, with options to minify and/or convert to Markdown.
+    """
+    output_dir = output_dir or os.getcwd()
+    os.makedirs(output_dir, exist_ok=True)
 
+    file_extension = "md" if convert_to_markdown else "html"
+    combined_file_path = os.path.join(output_dir, f"combined_1.{file_extension}")
+    combined_file = open(combined_file_path, "w")
     current_file_size = 0
     current_file_num = 1
-    os.makedirs(output_dir, exist_ok=True)
-    current_file_path = os.path.join(output_dir, f"combined_{current_file_num}.{'md' if convert_to_md else 'html'}")
-    current_file = open(current_file_path, "w")
 
     with ProcessPoolExecutor() as executor:
-        futures = []
-        for subdir, dirs, files in os.walk(input_dir):
-            for file in files:
-                if file.endswith(".html"):
-                    file_path = os.path.join(subdir, file)
-                    futures.append(executor.submit(process_file, file_path, minify, convert_to_md))
+        future_to_file = {
+            executor.submit(
+                process_file, os.path.join(subdir, file), minify, convert_to_markdown
+            ): file
+            for subdir, _, files in os.walk(input_dir)
+            for file in files
+            if file.endswith(".html")
+        }
 
-        for future in futures:
+        for future in future_to_file:
             content = future.result()
             content_size = len(content.encode("utf-8"))
+
             if current_file_size + content_size > max_size:
-                current_file.close()
+                combined_file.close()
                 current_file_num += 1
-                current_file_path = os.path.join(output_dir, f"combined_{current_file_num}.{'md' if convert_to_md else 'html'}")
-                current_file = open(current_file_path, "w")
+                combined_file_path = os.path.join(
+                    output_dir, f"combined_{current_file_num}.{file_extension}"
+                )
+                combined_file = open(combined_file_path, "w")
                 current_file_size = 0
 
-            current_file.write(content)
+            combined_file.write(content)
             current_file_size += content_size
 
-    current_file.close()
+    combined_file.close()
+
 
 @app.command()
 def main(
-    input_directory: str = typer.Argument(
-        ..., help="The input directory containing HTML files to combine."
-    ),
+    input_directory: str = typer.Argument(..., help="Input directory with HTML files."),
     output_directory: str = typer.Option(
-        None, help="The output directory where the combined file will be saved."
+        None, help="Output directory for combined files."
     ),
     max_file_size: int = typer.Option(
-        80 * 1024 * 1024, help="Maximum size of each combined file."
+        80 * 1024 * 1024, help="Maximum size of combined files."
     ),
     buffer_size: int = typer.Option(1024 * 1024, help="Buffer size for reading files."),
-    minify_html: bool = typer.Option(
-        False, "--minify", help="Enable minification of HTML files."
-    ),
+    minify_html: bool = typer.Option(False, "--minify", help="Minify HTML files."),
     convert_to_markdown: bool = typer.Option(
-        False, "--convert-md", help="Convert HTML files to Markdown format."
+        False, "--convert-md", help="Convert HTML to Markdown."
     ),
 ):
     """
-    Command-line interface to combine HTML files, with options to minify and convert to Markdown.
+    Command-line interface to combine, optionally minify, and/or convert HTML files to Markdown.
     """
     combine_files(
         input_directory,
