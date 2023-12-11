cfb266a283dda81a8b657f474b51ec6138e860e6
Pete Brooks <pete@txidigital.com>
2023-12-11 11:54:48 -0600

    [WIP] Add typer to html command

--- purr/clumps/text/html_combine.py
+++ purr/clumps/text/html_combine.py
@@ -1,9 +1,16 @@
 import os
-import sys
 import htmlmin
+import typer
+
+app = typer.Typer(invoke_without_command=True)
 
 
-def combine_html_files(input_dir, output_dir=None, max_size=80 * 1024 * 1024):
+def combine_html_files(
+    input_dir: str,
+    output_dir: str = None,
+    max_size: int = 80 * 1024 * 1024,
+    buffer_size: int = 1024 * 1024,
+):
     if output_dir is None:
         output_dir = os.getcwd()
 
@@ -44,11 +51,26 @@ def combine_html_files(input_dir, output_dir=None, max_size=80 * 1024 * 1024):
     current_file.close()
 
 
+@app.command()
+def main(
+    input_directory: str = typer.Argument(
+        ..., help="The input directory containing HTML files to combine."
+    ),
+    output_directory: str = typer.Option(
+        None, help="The output directory where the combined HTML file will be saved."
+    ),
+    max_file_size: int = typer.Option(
+        80 * 1024 * 1024, help="Maximum size of each combined HTML file."
+    ),
+    buffer_size: int = typer.Option(
+        1024 * 1024, help="Buffer size for reading HTML files."
+    ),
+):
+    """
+    This script combines multiple HTML files into a single file, minifying them in the process.
+    """
+    combine_html_files(input_directory, output_directory, max_file_size, buffer_size)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print(
-            "Usage: python combine_html.py <input-directory-path> [output-directory-path]"
-        )
-    else:
-        output_dir = sys.argv[2] if len(sys.argv) == 3 else None
-        combine_html_files(sys.argv[1], output_dir)
+    app()
