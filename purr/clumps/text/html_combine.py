df3fade8b062be27e54a977cf9ffab1f86dfb918
Pete Brooks <pete@txidigital.com>
2023-12-11 12:12:35 -0600

    [untested] use multiple processes to speed up

--- purr/clumps/text/html_combine.py
+++ purr/clumps/text/html_combine.py
@@ -1,22 +1,25 @@
 import os
 import htmlmin
 import typer
+from concurrent.futures import ProcessPoolExecutor
 from markdownify import markdownify as md
 from bs4 import BeautifulSoup
 
 app = typer.Typer(invoke_without_command=True)
 
-
 def html_to_markdown(html_content: str) -> str:
-    """
-    Converts HTML content to Markdown format.
-    """
-    soup = BeautifulSoup(html_content, "html.parser")
-    body = soup.find("body")
-    if body:
-        return md(str(body))
-    return md(html_content)
+    soup = BeautifulSoup(html_content, 'html.parser')
+    body = soup.find('body')
+    return md(str(body)) if body else md(html_content)
 
+def process_file(file_path: str, minify: bool, convert_to_md: bool) -> str:
+    with open(file_path, "r") as f:
+        content = f.read()
+        if minify:
+            content = htmlmin.minify(content, remove_empty_space=True)
+        if convert_to_md:
+            content = html_to_markdown(content)
+        return content
 
 def combine_files(
     input_dir: str,
@@ -26,48 +29,37 @@ def combine_files(
     minify: bool = False,
     convert_to_md: bool = False,
 ):
-    """
-    Combines files from a given directory into single files of a maximum size, with options to minify and convert to Markdown.
-    """
     if output_dir is None:
         output_dir = os.getcwd()
 
     current_file_size = 0
     current_file_num = 1
     os.makedirs(output_dir, exist_ok=True)
-    current_file_path = os.path.join(
-        output_dir, f"combined_{current_file_num}.{'md' if convert_to_md else 'html'}"
-    )
+    current_file_path = os.path.join(output_dir, f"combined_{current_file_num}.{'md' if convert_to_md else 'html'}")
     current_file = open(current_file_path, "w")
 
-    for subdir, dirs, files in os.walk(input_dir):
-        for file in files:
-            if file.endswith(".html"):
-                file_path = os.path.join(subdir, file)
+    with ProcessPoolExecutor() as executor:
+        futures = []
+        for subdir, dirs, files in os.walk(input_dir):
+            for file in files:
+                if file.endswith(".html"):
+                    file_path = os.path.join(subdir, file)
+                    futures.append(executor.submit(process_file, file_path, minify, convert_to_md))
 
-                with open(file_path, "r") as f:
-                    content = f.read()
-                    if minify:
-                        content = htmlmin.minify(content, remove_empty_space=True)
-                    if convert_to_md:
-                        content = html_to_markdown(content)
-                    content_size = len(content.encode("utf-8"))
+        for future in futures:
+            content = future.result()
+            content_size = len(content.encode("utf-8"))
+            if current_file_size + content_size > max_size:
+                current_file.close()
+                current_file_num += 1
+                current_file_path = os.path.join(output_dir, f"combined_{current_file_num}.{'md' if convert_to_md else 'html'}")
+                current_file = open(current_file_path, "w")
+                current_file_size = 0
 
-                    if current_file_size + content_size > max_size:
-                        current_file.close()
-                        current_file_num += 1
-                        current_file_path = os.path.join(
-                            output_dir,
-                            f"combined_{current_file_num}.{'md' if convert_to_md else 'html'}",
-                        )
-                        current_file = open(current_file_path, "w")
-                        current_file_size = 0
-
-                    current_file.write(content)
-                    current_file_size += content_size
+            current_file.write(content)
+            current_file_size += content_size
 
     current_file.close()
-
 
 @app.command()
 def main(
